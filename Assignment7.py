5659ecdbce9c963b1a1538ecd7cae3e25683e4eb
ReedMyers715 <reed.myers@bsu.edu>
2025-10-16 16:50:22 +0000

    Completed calculate_grade method

--- Assignment7.py
+++ Assignment7.py
@@ -1,2 +1,16 @@
 def calculate_grade(grade):
-    pass
+    if not isinstance(grade, (float, int)):
+        raise TypeError("Grade must be a number")
+    if grade < 0 or grade > 100:
+        raise ValueError("Grade must be between 0 and 100")
+    
+    if grade >= 90 and grade <= 100:
+        return "A"
+    if grade >= 80 and grade < 90:
+        return "B"
+    if grade >= 70 and grade < 80:
+        return "C"
+    if grade >= 60 and grade < 70:
+        return "D"
+    else:
+        return "F"
